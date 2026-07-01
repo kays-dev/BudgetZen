b38427a9116a1269ac34ea1b923ed1f5c578c988
kays-dev <krys.b@outlook.com>
2026-07-01 11:13:26 +0200

    General configuration of git repository

---
+++ BudgetZen/BudgetZenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetZenApp.swift
+//  BudgetZen
+//
+//  Created by Apprenant174 on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct BudgetZenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
